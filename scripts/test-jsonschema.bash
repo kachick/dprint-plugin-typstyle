76a3831f713051079d78a4887fbe86aeccac6ef9
Kenichi Kamiya <kachick1@gmail.com>
2025-02-23 17:36:15 +0900

    Replace jq with yq-go

--- scripts/test-jsonschema.bash
+++ scripts/test-jsonschema.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-jsonschema-cli deployment/schema.json --instance <(jq '.typst' ./tests/all/dprint.json)
+jsonschema-cli deployment/schema.json --instance <(yq --output-format json '.typst' ./tests/all/dprint.json)
