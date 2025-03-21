8d216b3cba3ad6ce091b719692398279ef6c5e2a
Kenichi Kamiya <kachick1@gmail.com>
2025-03-21 13:07:17 +0900

    Replace cargo-script with workspace bin for sharing Cargo.lock

--- scripts/normalize_json_schema.bash
+++ scripts/normalize_json_schema.bash
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 version="$(yq '.package.version' Cargo.toml)"
-./scripts/generate_json_schema.rs |
+cargo run --package generate_json_schema |
 	yq --output-format json "del(.required, .title) |
 		.\"\$id\" = \"https://plugins.dprint.dev/kachick/typstyle/${version}/schema.json\" |
 		.additionalProperties = false" >./deployment/schema.json
